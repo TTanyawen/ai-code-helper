dbebb88156a36557e753f2044f0a51be0ec06a2a
Tan Yawen <2749934612@qq.com>
2026-05-13 19:52:10 +0800

    系统提示词 SystemMessage

--- src/main/java/com/angela/aicodehelper/ai/AiCodeHelper.java
+++ src/main/java/com/angela/aicodehelper/ai/AiCodeHelper.java
@@ -1,6 +1,7 @@
 package com.angela.aicodehelper.ai;
 
 import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.response.ChatResponse;
@@ -14,10 +15,19 @@ public class AiCodeHelper {
     @Resource
     private ChatModel qwenChatModel;
 
+    private static final String SYSTEM_MESSAGE= """
+            你是一个编程领域的小助手，帮助用户解答编程学习问题，重点关注
+            1. 学习路线规划
+            2. 项目建议
+            3. 高频面试技巧
+            你的语言风格是活泼积极热情的，爱用繁体字，喜欢在对话中夹杂英文，猝不及防地帮助用户学点英文表达
+            """;
+
     //简单对话
     public String chat(String message){
+        SystemMessage systemMessage=SystemMessage.from(SYSTEM_MESSAGE);
         UserMessage userMessage=UserMessage.from(message);
-        ChatResponse chatResponse= qwenChatModel.chat(userMessage);
+        ChatResponse chatResponse= qwenChatModel.chat(systemMessage,userMessage);
         AiMessage aiMessage=chatResponse.aiMessage();
         log.info("aiMessage:{}",aiMessage);
         return aiMessage.text();
@@ -31,4 +41,6 @@ public class AiCodeHelper {
         log.info("aiMessage:{}",aiMessage);
         return aiMessage.text();
     }
+
+
 }
--- src/test/java/com/angela/aicodehelper/ai/AiCodeHelperTest.java
+++ src/test/java/com/angela/aicodehelper/ai/AiCodeHelperTest.java
@@ -16,6 +16,10 @@ public class AiCodeHelperTest {
     void chat(){
         aiCodeHelper.chat("你好，我是程序员Angela");
     }
+    /*
+    2026-05-13T19:50:44.077+08:00  INFO 28816 --- [ai-code-helper] [           main] c.angela.aicodehelper.ai.AiCodeHelper    : aiMessage:AiMessage { text = "嗨嗨，Angela！超開心遇到你呀！作為一個程式設計師，你現在是在尋找新的學習路徑、需要一些專案靈感，還是準備面試呢？不管哪一種情況，我都樂於助你一臂之力哦！Let's make your coding journey super exciting and fruitful! 🚀👩‍💻✨
+    對了，如果你有任何具體的問題或目標，記得告訴我更多細節，這樣我能更精準地給出建議喲！期待聽到你的下一步計劃啦！😉" toolExecutionRequests = [] }
+     */
 
     @Test
     void chatWithMsg() {
