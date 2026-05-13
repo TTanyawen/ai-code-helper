1bb0739f64eb1cb0ac6412f8fcd90559384a89dc
Tan Yawen <2749934612@qq.com>
2026-05-13 19:31:32 +0800

    多模态

--- src/main/java/com/angela/aicodehelper/ai/AiCodeHelper.java
+++ src/main/java/com/angela/aicodehelper/ai/AiCodeHelper.java
@@ -14,8 +14,18 @@ public class AiCodeHelper {
     @Resource
     private ChatModel qwenChatModel;
 
+    //简单对话
     public String chat(String message){
         UserMessage userMessage=UserMessage.from(message);
+        ChatResponse chatResponse= qwenChatModel.chat(userMessage);
+        AiMessage aiMessage=chatResponse.aiMessage();
+        log.info("aiMessage:{}",aiMessage);
+        return aiMessage.text();
+    }
+
+    //多模态，用户自定义消息类型
+    //千问模型不支持多模态
+    public String chatWithMsg(UserMessage userMessage){
         ChatResponse chatResponse= qwenChatModel.chat(userMessage);
         AiMessage aiMessage=chatResponse.aiMessage();
         log.info("aiMessage:{}",aiMessage);
--- src/test/java/com/angela/aicodehelper/ai/AiCodeHelperTest.java
+++ src/test/java/com/angela/aicodehelper/ai/AiCodeHelperTest.java
@@ -1,5 +1,8 @@
 package com.angela.aicodehelper.ai;
 
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,5 +15,14 @@ public class AiCodeHelperTest {
     @Test
     void chat(){
         aiCodeHelper.chat("你好，我是程序员Angela");
+    }
+
+    @Test
+    void chatWithMsg() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                ImageContent.from("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png")
+        );
+        aiCodeHelper.chatWithMsg(userMessage);
     }
 }
