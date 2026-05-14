e897114cb67ea9f794f316bc1fa9e3e8c638a392
Tan Yawen <2749934612@qq.com>
2026-05-14 18:32:50 +0800

    依赖帮忙自动注入AiService,省去自己写工厂类,方便但不灵活

--- src/main/java/com/angela/aicodehelper/ai/AICodeHelperService.java
+++ src/main/java/com/angela/aicodehelper/ai/AICodeHelperService.java
@@ -1,9 +1,16 @@
 package com.angela.aicodehelper.ai;
 
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.spring.AiService;
+
 /*
 AiService的声明式的写法
+以及
+自动注入的写法(无需自己写工厂类注入Bean),只需要加@AiService注解使得类被扫描
+
+但是,注解的方式牺牲灵活性,推荐使用工厂类注入的方式
  */
+@AiService
 public interface AICodeHelperService {
 
     @SystemMessage(fromResource = "system-prompt.txt")
--- src/main/java/com/angela/aicodehelper/ai/AiCodeHelperServiceFactory.java
+++ src/main/java/com/angela/aicodehelper/ai/AiCodeHelperServiceFactory.java
@@ -1,18 +1,18 @@
-package com.angela.aicodehelper.ai;
-
-import dev.langchain4j.model.chat.ChatModel;
-import dev.langchain4j.service.AiServices;
-import jakarta.annotation.Resource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AiCodeHelperServiceFactory {
-    @Resource
-    private ChatModel qwenChatModel;
-    @Bean
-    public AICodeHelperService AiCodeHelperService(){
-        //这里点进create可以看到反射创建AiService实现类
-        return AiServices.create(AICodeHelperService.class,qwenChatModel);
-    }
-}
+//package com.angela.aicodehelper.ai;
+//
+//import dev.langchain4j.model.chat.ChatModel;
+//import dev.langchain4j.service.AiServices;
+//import jakarta.annotation.Resource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class AiCodeHelperServiceFactory {
+//    @Resource
+//    private ChatModel qwenChatModel;
+//    @Bean
+//    public AICodeHelperService AiCodeHelperService(){
+//        //这里点进create可以看到反射创建AiService实现类
+//        return AiServices.create(AICodeHelperService.class,qwenChatModel);
+//    }
+//}
